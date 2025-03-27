478854f6b51896743d96aa8f4a42f650ae3b8d7f
danilo-gazzoli <dmartinsgazzoli@gmail.com>
2025-03-27 20:24:55 -0300

    feat(atividade): script consultando as inf de um usuario

---
+++ aula-27-03/main.py
@@ -0,0 +1,7 @@
+nome = input("Qual o seu nome?\n")
+
+residencia = input("\n onde voce mora? \n")
+
+idade = input("quantos anos voce tem? \n")
+
+print(f"Entendi, então voce é {nome} que tem {idade} anos de idade, e mora em {residencia}")
